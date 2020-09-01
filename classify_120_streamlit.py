3a06c1c34522d15dc6bdc8101af3166086666c4a
doubianimehdi <33954127+doubianimehdi@users.noreply.github.com>
2020-09-01 19:23:20 +0200

    Update classify_120_streamlit.py

--- classify_120_streamlit.py
+++ classify_120_streamlit.py
@@ -14,9 +14,9 @@ from io import BytesIO
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import array_to_img, img_to_array
 import tensorflow as tf
-<<<<<<< HEAD
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], False)
+
+#physical_devices = tf.config.experimental.list_physical_devices('GPU')
+#tf.config.experimental.set_memory_growth(physical_devices[0], False)
 
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -28,7 +28,6 @@ def load_model():
 @st.cache(allow_output_mutation=True)
 def load_model():
   model = tf.keras.models.load_model('dog_breed_classifier.h5')
->>>>>>> main
   return model
   
 @st.cache(allow_output_mutation=True)  
